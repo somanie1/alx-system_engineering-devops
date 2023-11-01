2ab9b09f593c93b93b84ffbab4f4b98c8b893b09
somanie1 <chisomonyiwa@gmail.com>
2023-11-01 11:37:08 -0700

    0x0A-configuration_management

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,11 @@
-#!/usr/bin/pup
-# Install a specific version of flask (2.1.0)
-  package {'flask':
-    ensure   => '2.1.0',
-    provider => 'pip3'
+# Installing flask and Werkzeug from pip3
+
+package { 'flask':
+ensure   => '2.1.0',
+provider => 'pip3',
+}
+
+package {'werkzeug':
+ensure   => '2.1.1',
+provider => 'pip3',
 }
